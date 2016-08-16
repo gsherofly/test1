9e6fa3db9303ef34ba952ec6297ebae7faf99b07
gaos <gaos@hnqingyun.com>
2016-08-16 15:29:14 +0800

    add file

---
+++ test1/HotViewController.h
@@ -0,0 +1,13 @@
+//
+//  HotViewController.h
+//  test1
+//
+//  Created by 青云 on 16/8/16.
+//  Copyright © 2016年 青云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotViewController : UIViewController
+
+@end
