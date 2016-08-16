869bc785186bb9515e176b661c00aab488a416fb
gaos <gaos@hnqingyun.com>
2016-08-16 15:02:46 +0800

    test1

---
+++ test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by 青云 on 16/8/16.
+//  Copyright © 2016年 青云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test1
+//
+//  Created by 青云 on 16/8/16.
+//  Copyright © 2016年 青云. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //....
+    NSLog(@"打印    ");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
